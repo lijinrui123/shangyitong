e69807ffdcf9b5c6d19d2e723f013a5860d4f142
WJD <2530622506@qq.com>
2023-07-07 09:54:23 +0800

    首页已有医院数据的TS类型定义

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -52,6 +52,7 @@ import Level from "./level/index.vue";
 import Region from "./region/index.vue";
 // 展示医院的卡片组件
 import Card from "./card/index.vue";
+import type { Content, hospitalResponseData } from "@/api/home/type";
 // 分页器需要的数据
 // import { ref } from "vue";
 // 分页器当前页面
@@ -59,9 +60,9 @@ let pageNo = ref<number>(1);
 // 一页展示几条数据
 let pageSize = ref<number>(5);
 // 存储已有的医院数据
-let hasHospitalArr = ref([]);
+let hasHospitalArr = ref<Content>([]);
 // 存储医院总个数
-let total = ref(0);
+let total = ref<number>(0);
 
 // 组件挂载完毕，发一次请求
 onMounted(() => {
@@ -70,7 +71,10 @@ onMounted(() => {
 // 获取已有的医院数据
 const getHospitalInfo = async () => {
   // 获取医院的数据：默认获取第一页、一页十个医院的数据
-  let result: any = await reqHospital(pageNo.value, pageSize.value);
+  let result: hospitalResponseData = await reqHospital(
+    pageNo.value,
+    pageSize.value
+  );
   if (result.code == 200) {
     // 存储已有的医院数据
     hasHospitalArr.value = result.data.content;
